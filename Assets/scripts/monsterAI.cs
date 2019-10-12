5e5ce475ad79664f7fa633af2b53b0fd9dbe3f65
rdt7867 <rdt7867@MSS2150-23>
2019-10-12 16:50:27 -0400

    intital

---
+++ Assets/scripts/monsterAI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class monsterAI : MonoBehaviour
+{
+    Transform target;
+    Vector3 dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        dir = new Vector3(0, 0, 0);
+
+        if (target.position.x < transform.position.x)
+            dir += new Vector3(-.02f, 0, 0);
+        if (target.position.x > transform.position.x)
+            dir += new Vector3(.02f, 0, 0);
+        if (target.position.y < transform.position.y)
+            dir += new Vector3(0, -.02f, 0);
+        if (target.position.y > transform.position.y)
+            dir += new Vector3(0, .02f, 0);
+
+        transform.position += dir;
+    }
+}
